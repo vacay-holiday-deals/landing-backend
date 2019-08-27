a7aacc595e1bb1cc9e60d389ea10b718a78d6b42
newtfrank <mymbugua@gmail.com>
2019-08-27 10:43:32 +0300

    added add user page and mongoengine schemas

--- README.md
+++ README.md
@@ -15,14 +15,14 @@ This command is for those using other environments such as [virtualenv](https://
 
 In your terminal where you have opened the app and the environment, run the command above to tell flask where the app is located
 
-## export FLASK_DEBUG=1
+### export FLASK_DEBUG=1
 
 Still in your terminal run the command to set debug mode to true
 
-## flask run
+### flask run
 
 This command will run you application in debug mode.
 
-## Using wsgi server
+### Using wsgi server
 
 For this the wsgi server of choice is gunicorn. To run gunicorn, you only need to call gunicorn and specify the application. Example `gunicorn run:app`, run is our application server script and app is the gunicorn app. So you are setting our app as gunicorn app
--- api/models.py
+++ api/models.py
@@ -4,10 +4,36 @@ from datetime import datetime
 
 
 # users db model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    username = db.Column(db.String(50))
-    role = db.Column(db.String(50))
-    password = db.Column(db.String(50))
+# class User(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100))
+#     email = db.Column(db.String(100))
+#     username = db.Column(db.String(50))
+#     role = db.Column(db.String(50))
+#     password = db.Column(db.String(50))
+
+
+class User(UserMixin, db.Document):
+    meta = {'collection': 'users'}
+    name = db.StringField(max_length=100)
+    email = db.EmailField()
+    username = db.StringField(max_length=100)
+    role = db.StringField(max_length=100)
+    password = db.StringField(min_length=6)
+    created = db.DateTimeField(default=datetime.now())
+
+
+class Offer(db.Document):
+    meta = {'collection': 'offers'}
+    Title = db.StringField(max_length=100)
+    Overview = db.StringField(max_length=2000)
+    Itinerary = db.StringField(max_length=2000)
+    Inclusion = db.StringField(max_length=2000)
+    Price = db.StringField(max_length=2000)
+    AddInfo = db.StringField(max_length=2000)
+    Images = db.ListField(db.StringField(), default=list)
+    Created = db.DateTimeField(default=datetime.now())
+
+
+class Email(db.Document):
+    meta = {'collection': 'emails'}
